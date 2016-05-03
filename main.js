7051511caa58b393560a10a0848d7e95015a8c36
Jordan Papaleo <papaleowebdev@gmail.com>
2016-05-02 18:27:35 -0700

    Created first part of the game

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-var history = []
+var colorHistory = ['red', 'yellow', 'blue', 'green']
 
+//
 function getRandomColor () {
   var colors = ['red', 'yellow', 'blue', 'green']
   var randomIndex = Math.floor(Math.random() * colors.length)
@@ -7,10 +8,39 @@ function getRandomColor () {
   return randomColor
 }
 
-function start () {
-  history = []
+function seedForTests (seedCount) {
+  while (seedCount > 0) {
+    colorHistory.push(getRandomColor())
+    seedCount--
+  }
 }
 
-function addColor () {
-  history.push(getRandomColor())
+function animate (colorSet) {
+  var i = 0
+  var interval = setInterval(function() {
+    turnOn(colorSet[i])
+
+    i++
+
+    if (i >= colorSet.length) {
+      clearInterval(interval);
+    }
+  }, 600)
 }
+
+function turnOn (color) {
+  var quad = document.querySelector('.' + color)
+  quad.classList.add('active')
+  setTimeout(function () {
+    quad.classList.remove('active')
+  }, 400)
+}
+
+function newGame () {
+  colorHistory = []
+  seedForTests(20)
+  animate(colorHistory)
+}
+
+var button = document.querySelector('button')
+button.addEventListener('click', newGame)
